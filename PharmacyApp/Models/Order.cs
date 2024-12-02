46550e607ebae83f0d120ad9501b21a70192b8bc
Hanane kh <143598467+Hanane8@users.noreply.github.com>
2024-12-02 12:41:52 +0100

    initial commit

---
+++ PharmacyApp/Models/Order.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PharmacyApp.Models
+{
+    public class Order
+    {
+        public string? CustomerName { get; set; }
+        public string? Address { get; set; }
+        public string? Email { get; set; }
+        public string? Phone { get; set; }
+        public ObservableCollection<CartItem> Items { get; set; }
+        public decimal? Total => Items.Sum(item => item.Total);
+
+        public override string ToString()
+        {
+            string orderDetails = $"Order for {CustomerName}\n" +
+                                  $"Address: {Address}\n" +
+                                  $"Email: {Email}\n" +
+                                  $"Phone: {Phone}\n\n" +
+                                  $"Items:\n";
+
+            foreach (var item in Items)
+            {
+                orderDetails += $"{item.Medication.Name} x {item.Quantity} = {item.Total:C}\n";
+            }
+
+            orderDetails += $"\nTotal: {Total:C}";
+            return orderDetails;
+        }
+    }
+
+}
